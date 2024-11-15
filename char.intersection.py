7b392f67ac5f4d5fd8d0a2eaf732145f79d5da68
Diogo Fonseca <diogofonseca.gamedesigner@gmail.com>
2024-11-15 13:17:46 +0000

    second try with help from teacher

--- char.intersection.py
+++ char.intersection.py
@@ -2,10 +2,8 @@ pl1=input()
 pl2=input()
 
 meio=set(pl1) and set(pl2)
-
-
-
 resultado=""
+
 
 for car in pl1:
     if car in meio and car not in resultado:
--- count.chars.py
+++ count.chars.py
@@ -1,10 +1,10 @@
-pl = input()
+pl=str(input)
 
-lista = pl.split(" ")
-ar={}
-for x in lista:
-    for y in x:
-        if y not in ar:
-            ar[x] = x.count(y)
-            
-            x.count(y)
+lista=pl.split(" ")
+
+dic={}
+
+for palavra in lista:
+    dic[palavra]=len(palavra)
+
+print (dic)
